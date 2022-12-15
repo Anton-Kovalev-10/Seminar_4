935b7c9d84d9e4951a95f3ae7f78a1854f2e5b74
Anton Kovalev <antoha91011@mail.ru>
2022-12-15 14:03:15 +0700

    Домашнее задание к семинару 4

---
+++ Задача 1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите первую цифру");
+string numberFirstStr = Console.ReadLine();
+int numberFirst = Convert.ToInt32(numberFirstStr);
+
+Console.WriteLine("Введите вторую цифру");
+string numberSecondtStr = Console.ReadLine();
+int numberSecond = Convert.ToInt32(numberSecondtStr);
+
+int x = numberFirst;
+int stepen = numberSecond;
+{
+    Console.WriteLine("степень первого числа равна " + Math.Pow(x, stepen));
+}
